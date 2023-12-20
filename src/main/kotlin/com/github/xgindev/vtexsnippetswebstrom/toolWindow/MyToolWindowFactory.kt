ac04918fbddc69b4ee1d9a23f26436cc50395466
GitHub Action <action@github.com>
2023-12-20 20:18:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/xgindev/vtexsnippetswebstrom/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xgindev/vtexsnippetswebstrom/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xgindev.vtexsnippetswebstrom.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xgindev.vtexsnippetswebstrom.MyBundle
+import com.github.xgindev.vtexsnippetswebstrom.services.MyProjectService
 import javax.swing.JButton
 
 
